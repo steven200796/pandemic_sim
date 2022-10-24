168a5dbac4db959dca31ce2bc3574de7473e1b9e
Steven Shi <steven200796@gmail.com>
2022-10-23 22:12:04 -0500

    Set restaurant ContactRateMatrix

--- python/pandemic_simulator/environment/location/restaurant.py
+++ python/pandemic_simulator/environment/location/restaurant.py
@@ -2,7 +2,7 @@
 
 from dataclasses import dataclass
 
-from ..interfaces import NonEssentialBusinessLocationState, ContactRate, SimTimeTuple, \
+from ..interfaces import NonEssentialBusinessLocationState, ContactRate, ContactRateMatrix, SimTimeTuple, \
     AgeRestrictedBusinessBaseLocation, NonEssentialBusinessBaseLocation
 
 __all__ = ['Bar', 'Restaurant', 'RestaurantState', 'BarState']
@@ -13,6 +13,31 @@ class RestaurantState(NonEssentialBusinessLocationState):
     contact_rate: ContactRate = ContactRate(1, 1, 0, 0.3, 0.35, 0.1)
     open_time: SimTimeTuple = SimTimeTuple(hours=tuple(range(11, 16)) + tuple(range(19, 24)),
                                            week_days=tuple(range(1, 7)))
+
+
+    # Chef, waiter, diner
+    # Rationale:
+    # Chefs work closely with other chefs, are fairly separated from the waiters, but touch the food of all diners
+    # Waiters have some exposure to other waiters, and more exposure to the diners
+    # Diners have minimal exposure to other diners, pending seating arrangements
+    """
+    contact_rate_matrix = [
+            [0.8, 0.2, 1],
+            [0.2, 0.6, 0.8],
+            [1, 0.8, 0.3]
+            ]
+            """
+
+    contact_rate_matrix = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 1]
+            ]
+    # 20% chefs 80% waiter breakdown for assignees
+    assignee_roles = [0.2, 0.8]
+    # all visitors are diners
+    visitor_roles = [1]
+    contact_rate_matrix: ContactRateMatrix = ContactRateMatrix(fraction_contact_matrix = contact_rate_matrix, assignee_roles = assignee_roles, visitor_roles = visitor_roles)
 
 
 class Restaurant(NonEssentialBusinessBaseLocation[RestaurantState]):
